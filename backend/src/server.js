0c2e91bf3c29a12517c2dfbf8450a9faa83eecdb
1817karthi <karthisanvi18@gmail.com>
2026-05-13 22:50:29 +0530

    Fixed CORS issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 connectDb();
 
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: "https://dreamy-tarsier-7c1a09.netlify.app/", credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
